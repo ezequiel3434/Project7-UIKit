0d828524b153724f5d247a94b9b0c3fb3ec1e270
ezequiel3434 <ezequiel3434@gmail.com>
2020-08-18 19:05:09 -0300

    Parsing JSON using the Codable protocol

---
+++ Project7-UIKit/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7-UIKit
+//
+//  Created by Ezequiel Parada Beltran on 18/08/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project7-UIKit/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7-UIKit
+//
+//  Created by Ezequiel Parada Beltran on 18/08/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
