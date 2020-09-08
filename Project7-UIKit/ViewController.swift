0edfe17aa1774005fec014e9d879b54300398187
ezequiel3434 <ezequiel3434@gmail.com>
2020-09-08 19:42:47 -0300

    2-Let users filter the petitions they see: Done

--- Project7-UIKit/ViewController.swift
+++ Project7-UIKit/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for news..."
+        return searchBar
+    }()
+    
+    
+    var petitionsResult = [Petition]()
+    
     var petitions = [Petition]()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,13 +26,20 @@ class ViewController: UITableViewController {
         let credits = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
         let urlString: String
         navigationItem.rightBarButtonItem = credits
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        
+        searchBar.delegate = self
+        // searchBar.becomeFirstResponder()
+        
         if navigationController?.tabBarItem.tag == 0 {
+            
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
         } else {
+            print("tops")
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
-        
+        navigationController?.tabBarController?.delegate = self
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
@@ -34,6 +50,14 @@ class ViewController: UITableViewController {
         showError()
         
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        searchBar.text = nil
+        //            searchBar.resignFirstResponder()
+        petitionsResult = petitions
+        tableView.reloadData()
     }
     
     func showError() {
@@ -52,18 +76,19 @@ class ViewController: UITableViewController {
         let decoder = JSONDecoder()
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
             petitions = jsonPetitions.results
+            petitionsResult = petitions
             tableView.reloadData()
         }
     }
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return petitionsResult.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = petitionsResult[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -71,10 +96,75 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = petitionsResult[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
         
     }
-
+    
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+            return
+        }
+        searchBar.resignFirstResponder()
+        petitionsResult.removeAll()
+        
+        filterUsers(with: text)
+    }
+    
+    
+    
+    func filterUsers(with term: String) {
+        
+        // update the UI: either show results or show no results label
+        
+        
+        let results: [Petition] = petitions.filter({
+            
+            let title = $0.title.lowercased()
+            
+            
+            
+            
+            return title.contains(term.lowercased())
+        }).compactMap({
+            let title = $0.title
+            let body = $0.body
+            let signatureCount = $0.signatureCount
+            return Petition(title: title, body: body, signatureCount: signatureCount)
+        })
+        
+        self.petitionsResult = results
+        
+        tableView.reloadData()
+        //updateUI()
+    }
+    
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            petitionsResult = petitions
+            tableView.reloadData()
+        }
+    }
+}
+
+extension ViewController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        let tabBarIndex = tabBarController.selectedIndex
+        if tabBarIndex == 0 {
+            print(viewController)
+            searchBar.text = nil
+            //            searchBar.resignFirstResponder()
+            petitionsResult = petitions
+            tableView.reloadData()
+            
+        }else {
+            print(viewController)
+            
+        }
+    }
+}
